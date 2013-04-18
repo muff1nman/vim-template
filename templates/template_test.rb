0a95150fc6656fb84b1b3dd2a6337e437ea6905b
Andrew DeMaria <ademaria@mines.edu>
2013-04-17 19:41:38 -0600

    New test template

---
+++ templates/template_test.rb
@@ -0,0 +1,12 @@
+#! /usr/bin/env ruby
+#
+# Copyright © 2013 Andrew DeMaria (muff1nman) <ademaria@mines.edu>
+#
+# All Rights Reserved.
+
+require 'test/unit'
+
+class %HERE% < Test::Unit::TestCase
+
+end
+
